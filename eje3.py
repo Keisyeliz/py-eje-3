01058c685bc5f4e4b08ee1a0dd783b36a45f6210
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-02-22 17:46:48 -0500

    Add files via upload

---
+++ eje3.py
@@ -0,0 +1,19 @@
+# Un estudiante debe realizar su proceso de matritula teniendo en cuenta los siguiente:
+#monto:?
+
+
+cons_credito1Str = 28000
+cons_credito2Str = 35000
+cons_credito3Str = 40000
+cons_credito4Str = 45000
+cons_credito5Str = 55000
+cons_credito6Str = 60000
+cons_credito7Str = 75000
+cons_credito8Str = 80000
+cons_credito9Str = 95000
+cons_credito10Str = 110000
+
+var_nombreStr = input('Nombre')
+var_contactoStr = input('Contacto')
+var_montoFlt = float(input('Monto disponible ->'))
+
